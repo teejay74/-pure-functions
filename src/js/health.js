2a72e61f9d52209e6117f8f500cd5acdfc159ee5
teejay74 <teejay744@gmail.com>
2020-09-23 16:42:37 +0500

    test husky

--- src/js/health.js
+++ src/js/health.js
@@ -1,5 +1,5 @@
 export default function getHealthPoints(hp) {
-    if (hp.health > 50) return "healthy"
-    else if (hp.health < 15) return "critical"
-    else return "wounded"
+  if (hp.health > 50) return 'healthy';
+  if (hp.health < 15) return 'critical';
+  return 'wounded';
 }
